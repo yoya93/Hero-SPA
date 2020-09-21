d5dd1c18519b275a2e54d40022d15b2bcbc9f490
jorge <jorgelopezgonzalez474@gmail.com>
2020-09-21 09:15:30 -0300

    docs Added

---
+++ docs/precache-manifest.c017f8a10eb43e008a493463383a5eb4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0b56756a003a52ef449b133f14b2202",
+    "url": "/index.html"
+  },
+  {
+    "revision": "591aca00b6073d515eff",
+    "url": "/static/js/2.3a10d2da.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3a10d2da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97ca54a0e1b9cc7f8220",
+    "url": "/static/js/main.677cc24c.chunk.js"
+  },
+  {
+    "revision": "94d7ca25d4df19621824",
+    "url": "/static/js/runtime-main.b7f813af.js"
+  }
+]);
